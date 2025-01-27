0a6784d966c67a70169a90733a279a13f1a5fde7
DAVID ANDRE ACOSTA AVILA <196404@iberopuebla.mx>
2025-01-27 12:43:00 -0600

    Arquitectura

---
+++ AgriConnect.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AgriConnect.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
