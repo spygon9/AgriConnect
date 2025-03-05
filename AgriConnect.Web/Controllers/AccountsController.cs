16f02f3027eb24213966a13153267c0b391b6048
DAVID ANDRE ACOSTA AVILA <196404@iberopuebla.mx>
2025-03-05 12:44:04 -0600

    Creación de la vista Login Creación del método Get en login Creación del método Post en login Cambios en la vista de Shared/Layout

--- AgriConnect.Web/Controllers/AccountsController.cs
+++ AgriConnect.Web/Controllers/AccountsController.cs
@@ -19,16 +19,46 @@ namespace AgriConnect.Web.Controllers
             this.userHelper = userHelper;
             this.configuration = configuration;
         }
-        [HttpPost("Login")]
-        public async Task<ActionResult> Login([FromBody]LoginDTO loginDTO)
+        //[HttpPost("Login")]
+        //public async Task<ActionResult> Login([FromBody]LoginDTO loginDTO)
+        //{
+        //    var result = await userHelper.LoginAsync(loginDTO);
+        //    if (result.Succeeded)
+        //    {
+        //        var user = await userHelper.GetUserAsync(loginDTO.Email);
+        //        return Ok(user);
+        //    }
+        //    return BadRequest("Usuario o contraseña incorrecta");
+        //}
+
+        //GET
+        [HttpGet]
+        public IActionResult Login()
         {
-            var result = await userHelper.LoginAsync(loginDTO);
-            if (result.Succeeded)
+            if(User.Identity.IsAuthenticated)
             {
-                var user = await userHelper.GetUserAsync(loginDTO.Email);
-                return Ok(user);
+                return RedirectToAction(nameof(Index),"Home");
             }
-            return BadRequest("Usuario o contraseña incorrecta");
+            return View(new LoginDTO());
+        }
+        //POST
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginDTO loginDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await userHelper.LoginAsync(loginDTO);
+                if (result.Succeeded)
+                {
+                    if(Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrecta");
+            }
+            return View(loginDTO);
         }
         private object? BuildToken(User user)
         {
